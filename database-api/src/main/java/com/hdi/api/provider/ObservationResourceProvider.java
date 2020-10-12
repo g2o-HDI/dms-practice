6cd1c66f1f88b9ff6b8d5ee023c14ee3c93cd2c3
ICCOHIO\tfrazier <travis.frazier@g2o.com>
2020-10-12 15:40:58 -0400

    Changes to the observation Provider to handle A1C

--- database-api/src/main/java/com/hdi/api/provider/ObservationResourceProvider.java
+++ database-api/src/main/java/com/hdi/api/provider/ObservationResourceProvider.java
@@ -1,14 +1,13 @@
 package com.hdi.api.provider;
 
-import ca.uhn.fhir.rest.annotation.IdParam;
-import ca.uhn.fhir.rest.annotation.Read;
-import ca.uhn.fhir.rest.annotation.RequiredParam;
-import ca.uhn.fhir.rest.annotation.Search;
+import ca.uhn.fhir.rest.annotation.*;
+import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.param.StringParam;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 import com.hdi.api.database.OpenERMDatabase;
 import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.Observation;
+import org.hl7.fhir.dstu3.model.OperationOutcome;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 
@@ -39,5 +38,25 @@ public class ObservationResourceProvider implements IResourceProvider {
         return db.retrieveObservationsByCode(patientId.getIdPart(),code[1]);
     }
 
+    @Create()
+    public MethodOutcome createObservation(@ResourceParam Observation observation){
+        // Save this patient to the database...
+        OpenERMDatabase db = new OpenERMDatabase();
+        String newObservationId = db.addNewObservation(observation);
+
+        //return the new Id if success else return an error message
+        MethodOutcome retVal = new MethodOutcome();
+        if (newObservationId == null) {
+            retVal.setId(new IdType("Observation", newObservationId, "1.0"));
+            retVal.setCreated(true);
+        }else {
+            OperationOutcome outcome = new OperationOutcome();
+            outcome.addIssue().setDiagnostics("An Error Occurred");
+            retVal.setOperationOutcome(outcome);
+            retVal.setCreated(true);
+        }
+        
+        return retVal;
+    }
 
 }
